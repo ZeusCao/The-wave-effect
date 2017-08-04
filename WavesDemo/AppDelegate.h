adbcb2249efd703956f0fb83f44888d1fb88ddec
曹檑 <1090491594@qq.com>
2017-08-04 10:03:55 +0800

    Initial Commit

---
+++ WavesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavesDemo
+//
+//  Created by Zeus on 2017/8/4.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
